55e7ebe8157867158144620f4173e661770c28bc
ahah2025 <adghty61@naver.com>
2025-05-22 09:53:43 +0900

    2025-05

--- src/com/javaex/ex01/MainApp.java
+++ src/com/javaex/ex01/MainApp.java
@@ -13,17 +13,22 @@ public class MainApp {
 		int num = sc.nextInt();
 		
 		if(num>0) {
+			
 			//양수 일때만
-			if(num%2==0) { //num%2 == 0 짝수니? 
+			if(num % 2 == 0) { //num%2 == 0 짝수니? 
 				//짝수면
 				System.out.println(num+" 는 짝수입니다.");
 			}else { //홀수 입력할때
 				System.out.println(num+" 는 홀수입니다.");
 			}
+			
+			
 		}else if(num<0) {
 			System.out.println(num+" 는 0보다 작은 수 입니다.");
+		
 		}else {
 			System.out.println(num+" 는 짝수입니다.");
+
 		}
 		sc.close();
 	}
--- src/com/javaex/ex02/Gugudan.java
+++ src/com/javaex/ex02/Gugudan.java
@@ -12,20 +12,11 @@ public class Gugudan {
 
 		int dan = sc.nextInt();
 
-		for (int i = 1; i <= 9; i++) {
-			System.out.println(dan + "*" + i + "=" + dan * i);
-
-			if (dan <= 2) {
-
-			} else if (dan <= 9) {
-
+		if(dan>=2 && dan <=9) {
+			for (int i = 1; i <= 9; i++) {
+				System.out.println(dan + "*" + i + "=" + dan * i);
 			} 
-		}
 		
-		if(dan != 13) { //!= 입력 x 
-			
-		}else if(dan == -5)	{
-
 		}else {
 			System.out.println("2단에서 9단까지만 출력가능합니다");
 		}
@@ -33,17 +24,3 @@ public class Gugudan {
 		sc.close();
 	}
 }
-/*
-단을 입력하세요
-단:13
-13*1=13
-13*2=26
-13*3=39
-13*4=52
-13*5=65
-13*6=78
-13*7=91
-13*8=104
-13*9=117
-2단에서 9단까지만 출력가능합니다
-*/
